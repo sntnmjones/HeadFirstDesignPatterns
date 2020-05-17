d8721fd2d06b4c030275181a323904366dde2338
Troy Jones <sntnmjones@gmail.com>
2020-05-17 09:38:55 -0700

    Functioning Strategy Pattern Implementation

--- chapt2/Subject.java
+++
@@ -1,7 +0,0 @@
-package chapt2;
-
-public interface Subject {
-    public void registerObserver(Observer observer);
-    public void removeObserver(Observer observer);
-    public void notifyObservers();
-}
